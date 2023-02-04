81ccde9e0ec6aa0e5ace09e77578ca7f85ce7c92
sorcros <sorcros@mail.ru>
2023-02-04 12:44:32 +0300

    Initial Commit

---
+++ Tinkoff-API/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tinkoff-API
+//
+//  Created by Диана Гатауллина on 04.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tinkoff-API/Tinkoff_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinkoff_APIApp.swift
+//  Tinkoff-API
+//
+//  Created by Диана Гатауллина on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tinkoff_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
