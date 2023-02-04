4352db5deaa7cbe2dcc0527cf678e0d21c484ef2
sorcros <sorcros@mail.ru>
2023-02-04 13:10:16 +0300

    first-commit Tinkoff-test-task

--- Tinkoff-API/ContentView.swift
+++ Tinkoff-API/ContentView.swift
@@ -1,21 +1,74 @@
-//
-//  ContentView.swift
-//  Tinkoff-API
-//
-//  Created by Диана Гатауллина on 04.02.2023.
-//
+import UIKit
 
-import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+struct News: Decodable {
+  let title: String
+  let description: String
+  let url: String
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class NewsViewController: UITableViewController {
+
+    var news = [News]()
+    
+    var selectedNews: News?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchNews()
     }
+
+    func fetchNews() {
+        // Выполняем API-запрос для получения списка новостей
+        let url = URL(string: "https://your-api.com/news")!
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let data = data else { return }
+
+            // Распарсиваем полученные данные
+            let decoder = JSONDecoder()
+            do {
+                let newsResponse = try decoder.decode([News].self, from: data)
+                self?.updateUI(with: newsResponse)
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }
+        task.resume()
+    }
+
+    func updateUI(with news: [News]) {
+        DispatchQueue.main.async { [weak self] in
+            self?.news = news
+            self?.tableView.reloadData()
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return news.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath)
+        let newsItem = news[indexPath.row]
+        cell.textLabel?.text = newsItem.title
+        cell.detailTextLabel?.text = newsItem.description
+        return cell
+    }
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowNewsDetail" {
+            let destinationVC = segue.destination as! NewsViewController
+            let selectedIndex = tableView.indexPathForSelectedRow!
+            destinationVC.selectedNews = news[selectedIndex.row]
+        }
+    }
+
 }
+
+
+
